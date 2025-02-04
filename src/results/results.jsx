e74a2ed33ff2a1ae16e09d5d5f135318dc59c231
Spencer <spencerglade@gmail.com>
2025-02-04 11:45:41 -0700

    results and vote inject

--- src/results/results.jsx
+++ src/results/results.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import './results.css';
 
 export function Results() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>
-        
-      </div>
+    <main>
+        <h2>Results</h2>
+            <ul class="updates">
+                <li>"Wet" from "Is Water Wet?" Is Winning!</li>
+                <li>"Does Pineapple Go on Pizza?" Is Tied!</li>
+                <li>"Gif" from "Is it Pronounced "Gif" or "Jif"?" Is Winning!</li>
+                
+            </ul>
+
+
+        <h3>What Utensil Do You Eat Macaroni and Cheese With?</h3>
+        <ul>
+            <p>Fork: 8</p>
+            <p>Spoon: 2</p>
+        </ul>
+
+        <h3>Is Water Wet?</h3>
+        <ul>
+            <p>Wet: 6</p>
+            <p>Not Wet: 4</p>
+        </ul>
+
+        <h3>Is Cereal A Soup?</h3>
+        <ul>
+            <p>Soup: 3</p>
+            <p>Not Soup: 7</p>
+        </ul>
+
+        <h3>Does Pineapple Go on Pizza?</h3>
+        <ul>
+            <p>Heck Yes: 5</p>
+            <p>Absolutely Not: 5</p>
+        </ul>
+
+        <h3>Is Gif pronounced "Gif" or "Jif" ?</h3>
+        <ul>
+            <p>Gif: 6</p>
+            <p>Jif: 4</p>
+        </ul>
+
+        <h3>Are Panckaes or Waffles Better?</h3>
+        <ul>
+            <p>Pancakes: 7</p>
+            <p>Waffles: 3</p>
+        </ul>
     </main>
   );
 }
--- src/vote/vote.jsx
+++ src/vote/vote.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
+import './vote.css';
 
 export function Vote() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>
+    <main>
+        <h2>Select Your Choice</h2>
+            <ul class="radio">
+                <label class="q" for="text">What Utensil Do You Use With Mac N Cheese?</label>
+                <div> <img src="macncheese.png" alt="mac-n-cheese" width="500" /></div>
+                <input type="radio" name="mac" value="fork" required />
+                <label for="fork">Fork</label>
+                <input type="radio" name="mac" value="spoon" required />
+                <label for="spoon">Spoon</label>
+            </ul>
+            <ul class="radio">
+                <label class="q" for="text">Is Water Wet?</label>
+                <div> <img src="water.png" alt="water" width="500" height="300" /></div>
+                <input type="radio" name="water" value="wet" required />
+                <label for="wet">Wet</label>
+                <input type="radio" name="water" value="notwet" required />
+                <label for="notwet">Not Wet</label>
+            </ul>
+            <ul class="radio">
+                <label class="q" for="text">Is Cereal A Soup?</label>
+                <div> <img src="cereal.png" alt="cereal" width="500" height="300" /></div>
+                <input type="radio" name="cereal" value="soup" required />
+                <label for="soup">Soup</label>
+                <input type="radio" name="cereal" value="notsoup" required />
+                <label for="notsoup">Not Soup</label>
+            </ul>
+            <ul class="radio">
+                <label class="q" for="text">Does Pineapple Go on Pizza?</label>
+                <div> <img src="pineapplepizza.png" alt="Pineapple Pizza" width="500" height="300" /></div>
+                <input type="radio" name="pineapple" value="yes" required />
+                <label for="yes">Heck Yes</label>
+                <input type="radio" name="pineapple" value="no" required />
+                <label for="no">Absolutely Not</label>
+            </ul>
+            <ul class="radio">
+                <label class="q" for="text">Is Gif pronounced "Gif" or "Jif" ?</label>
+                <div> <img src="https://data.textstudio.com/output/sample/animated/3/9/4/7/gif-5-17493.gif" alt="Gif" width="500" height="300" /></div>
+                <input type="radio" name="gif" value="gif" required />
+                <label for="gif">Gif</label>
+                <input type="radio" name="gif" value="jif" required />
+                <label for="jif">Jif</label>
+            </ul>
+            <ul class="radio">
+                <label class="q" for="text">Are Panckaes or Waffles Better?</label>
+                <div> <img src="breakfast.png" alt="Pancakes and Waffles" width="500" height="300" /></div>
+                <input type="radio" name="pancake" value="pancake" required />
+                <label for="pancake">Pancakes</label>
+                <input type="radio" name="pancake" value="waffle" required />
+                <label for="waffle">Waffles</label>
+            </ul>
 
-
-      </div>
+            <form method="get" action="results.html">
+                <button type="submit">Submit Votes</button>
+            </form>
     </main>
   );
 }
