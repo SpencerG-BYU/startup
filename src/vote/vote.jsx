fc2314043688abed7530873a0498a52c6ab3712d
Spencer <spencerglade@gmail.com>
2025-02-26 21:28:06 -0700

    questions data struc

--- src/vote/vote.jsx
+++ src/vote/vote.jsx
@@ -4,6 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 export function Vote() {
+    const questions = [
+        {question: "What Utensil Do You Use With Mac N Cheese?", options: ["Fork", "Spoon"], image: "macncheese.png"},
+        {question: "Is Water Wet?", options: ["Wet", "Not Wet"], image: "water.png"},
+        {question: "Is Cereal A Soup?", options: ["Soup", "Not Soup"], image: "cereal.png"},
+        {question: "Does Pineapple Go on Pizza?", options: ["Heck Yes", "Absolutely Not"], image: "pineapplepizza.png"},
+        {question: "Is Gif pronounced 'Gif' or 'Jif' ?", options: ["Gif", "Jif"], image: "https://data.textstudio.com/output/sample/animated/3/9/4/7/gif-5-17493.gif"},
+        {question: "Are Panckaes or Waffles Better?", options: ["Pancakes", "Waffles"], image: "breakfast.png"}
+    ];
+
   return (
     <main>
         <h2>Select Your Choice</h2>
