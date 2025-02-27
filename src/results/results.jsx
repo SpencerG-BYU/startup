60f5b4ddd136443299ae92499abbd4bfd4d8c0f0
Spencer <spencerglade@gmail.com>
2025-02-27 01:19:26 -0700

    websocket placeholder

--- src/results/results.jsx
+++ src/results/results.jsx
@@ -6,12 +6,13 @@ import './results.css';
 export function Results() {
     const [results, setResults] = React.useState({});
     const navigate = useNavigate();
+    const [msg, setMsg] = React.useState('...listening');
     const questions = [
-        {question: "What Utensil Do You Use With Mac N Cheese?", options: ["Fork", "Spoon"], image: "macncheese.png"},
+        {question: "What Utensil Do You Eat Mac N Cheese With?", options: ["Fork", "Spoon"], image: "macncheese.png"},
         {question: "Is Water Wet?", options: ["Wet", "Not Wet"], image: "water.png"},
         {question: "Is Cereal A Soup?", options: ["Soup", "Not Soup"], image: "cereal.png"},
         {question: "Does Pineapple Go on Pizza?", options: ["Heck Yes", "Absolutely Not"], image: "pineapplepizza.png"},
-        {question: "Is Gif pronounced 'Gif' or 'Jif' ?", options: ["Gif", "Jif"], image: "https://data.textstudio.com/output/sample/animated/3/9/4/7/gif-5-17493.gif"},
+        {question: "Is Gif pronounced 'Gif' or 'Jif'?", options: ["Gif", "Jif"], image: "https://data.textstudio.com/output/sample/animated/3/9/4/7/gif-5-17493.gif"},
         {question: "Are Panckaes or Waffles Better?", options: ["Pancakes", "Waffles"], image: "breakfast.png"}
     ];
 
@@ -26,9 +27,35 @@ export function Results() {
         return Object.values(results).filter(vote => vote === option).length;
     };
 
+    React.useEffect(() => {
+        const intervalId = setInterval(() => {
+            const randomQuestionIndex = Math.floor(Math.random() * questions.length);
+            const randomQuestion = questions[randomQuestionIndex];
+            const randomOptionIndex = Math.floor(Math.random() * randomQuestion.options.length);
+            const randomOption = randomQuestion.options[randomOptionIndex];
+            const otherOption = randomQuestion.options[(randomOptionIndex + 1) % randomQuestion.options.length];
+
+            const randomOptionVotes = countVotes(randomQuestion.question, randomOption);
+            const otherOptionVotes = countVotes(randomQuestion.question, otherOption);
+
+            if (randomOptionVotes > otherOptionVotes) {
+                setMsg(`"${randomOption}" overtook "${otherOption}" from "${randomQuestion.question}"`);
+            } 
+            else if (randomOptionVotes == otherOptionVotes) {
+                setMsg(`"${randomOption}" and "${otherOption}" are tied from "${randomQuestion.question}"`);
+            } else {
+                setMsg(`"${otherOption}" is still leading from "${randomQuestion.question}"`);
+            }
+        }, 7000);
+
+        return () => clearInterval(intervalId);
+    }, [results]);
+            
+
     return (
         <main>
             <h2>Results</h2>
+            <div className="updates">{msg}</div>
             {questions.map((question, index) => (
                 <div key={index}>
                     <h3>{question.question}</h3>
